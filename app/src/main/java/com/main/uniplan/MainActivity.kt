606cbf87addc75032ec8249aa835229eb13e5dfb
andre <pigliafreddoandrea.ap@gmail.com>
2024-04-16 18:03:25 +0200

    Funzionamento Bottoni

--- app/src/main/java/com/main/uniplan/MainActivity.kt
+++ app/src/main/java/com/main/uniplan/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.main.uniplan
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.uniplan.Plan
 import com.example.uniplan.R
+import com.example.uniplan.Summaries
+import java.util.Calendar
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +23,20 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    fun Plan(view: View?) {
+        val intent = Intent(this, Plan::class.java)
+        startActivity(intent)
+    }
+
+    //rifaccio calendar, errore ed esce dall'app
+    fun Calendar(view: View?) {
+        val intent = Intent(this, Calendar::class.java)
+        startActivity(intent)
+    }
+
+    fun mySummaries(view: View?) {
+        val intent = Intent(this, Summaries::class.java)
+        startActivity(intent)
     }
 }
