d9758a48816473c8d350ad494c10d49bc9420ed6
barbioo <lorenzo2002.barbieri@gmail.com>
2024-04-24 16:46:27 +0200

    Aggiunto scorrimento

--- app/src/main/java/com/main/uniplan/MainActivity.kt
+++ app/src/main/java/com/main/uniplan/MainActivity.kt
@@ -1,29 +1,44 @@
 package com.main.uniplan
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.TextField
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.builder.api.RequestBuilder
+import com.builder.api.RequestSender
+import com.builder.api.RequestsFileManager
 import com.example.uniplan.Plan
 import com.example.uniplan.R
 import com.example.uniplan.Summaries
+import kotlinx.serialization.json.JsonBuilder
 import java.util.Calendar
 
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            val params = v.layoutParams as ViewGroup.MarginLayoutParams
+            params.topMargin = systemBars.top
+            v.layoutParams = params
             insets
         }
+
+
+
     }
+
     fun Plan(view: View?) {
         val intent = Intent(this, Plan::class.java)
         startActivity(intent)
